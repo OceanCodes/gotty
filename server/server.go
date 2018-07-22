f62ea87f8821e3199d1b08f46500865ecdc74116
shay <shay@codeocean.com>
2018-07-22 12:37:26 +0300

    pr fixes

--- server/server.go
+++ server/server.go
@@ -98,8 +98,8 @@ func (server *Server) Run(ctx context.Context, options ...RunOption) error {
 	path := "/"
 	if server.options.EnableRandomUrl {
 		path = "/" + randomstring.Generate(server.options.RandomUrlLength) + "/"
-	} else if server.options.CustomURL != "" {
-		path = "/" + server.options.CustomURL + "/"
+	} else if server.options.Path != "" {
+		path = "/" + server.options.Path + "/"
 	}
 
 	handlers := server.setupHandlers(cctx, cancel, path, counter)
